d03c255e86862ded83b7cc2fb6633495c5af28e7
RahilVaseer <rahil.vaseer@gmail.com>
2019-10-15 17:15:46 +0200

    Add PostgeSQL support

--- src/test/java/no/kristiania/projectManagement/TaskTest.java
+++ src/test/java/no/kristiania/projectManagement/TaskTest.java
@@ -1,16 +1,24 @@
 package no.kristiania.projectManagement;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class taskTest{
+public class TaskTest {
 
     @Test
-    void shouldRetriveStoredData(){
-        taskDao dao = new taskDao();
+    void shouldRetriveStoredData() throws SQLException {
+        final JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+
+        dataSource.getConnection().createStatement().executeUpdate(
+                "create table tasks (name varchar(100))"
+        );
+        TaskDao dao = new TaskDao(dataSource);
         String taskName = pickOne (new String[] {"change status", "get files", "add new project members"});
         dao.insertTask(taskName);
         assertThat(dao.listAll()).contains(taskName);
@@ -19,6 +27,7 @@ public class taskTest{
 
     private String pickOne(String[] strings) {
         return strings[new Random().nextInt(strings.length)];
+
     }
 
 
